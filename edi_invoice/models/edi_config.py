d763783518b8ca17ea76911ecb612b7deb413c13
Prashant Panchal <ppa@odoo.com>
2020-01-07 10:45:04 +0530

    [IMP] pep8 format the code

--- edi_invoice/models/edi_config.py
+++ edi_invoice/models/edi_config.py
@@ -4,15 +4,14 @@
 import os
 import pprint
 import logging
-import traceback
 import tempfile
-from lxml import etree
 
-from odoo import fields, models, _
+from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 pp = pprint.PrettyPrinter(indent=4)
 EDI_DATE_FORMAT = '%m/%d/%Y'
+
 
 class SyncDocumentType(models.Model):
 
@@ -20,20 +19,19 @@ class SyncDocumentType(models.Model):
 
     doc_code = fields.Selection(selection_add=[
                                 ('export_invoice_flat', '810 - Export Invoice (TrueCommerce Flatfile)'),
-                            ])
+                                ])
     segment_terminator = fields.Char(string='Segment Terminator',
                         help='Symbol representing segment terminator delimiter, Industry practice: return/linefeed (\\r\\n")',
                         default='\\r\\n'
-                    )
+                                     )
     data_ele_separator = fields.Char(string='Data Element Separator',
                         help='Symbol representing data element separator. Industry practice : asterisk (*), tilde (~)',
                         default='*'
-                    )
+                                     )
     component_ele_separator = fields.Char(string='Component Element Separator',
                         help='Symbol representing Component element separator. Industry practice : colon (:)',
                         default=':'
-                    )
-
+                                          )
 
     def _do_export_so(self, conn, sync_action_id, values):
         '''
@@ -49,8 +47,8 @@ class SyncDocumentType(models.Model):
         return True
 
     def make_invoice_line_flatfile_data(self, invoice):
-        join_seg_ele = '%s'%(self.segment_terminator)
-        join_data_ele = '%s'%(self.data_ele_separator)
+        join_seg_ele = '%s' % (self.segment_terminator)
+        join_data_ele = '%s' % (self.data_ele_separator)
         invoice_line_element = []
         line_count = 1
         for line in invoice.invoice_line_ids:
@@ -64,14 +62,14 @@ class SyncDocumentType(models.Model):
                 line.quantity,
                 line.product_uom_id.name or '',
                 line.price_unit,
-                sum(line.mapped('sale_line_ids.product_uom_qty')) or line.quantity, 
-                0, # Number of packs in unit
-                0, # Number of inner packs within the master pack
-                0, # Line item allowance percentage
-                0, # Line item allowance dollar amount
+                sum(line.mapped('sale_line_ids.product_uom_qty')) or line.quantity,
+                0,  # Number of packs in unit
+                0,  # Number of inner packs within the master pack
+                0,  # Line item allowance percentage
+                0,  # Line item allowance dollar amount
             ]
-            line_data = join_data_ele.join([ str(el) for el in line_data_elements])
-            line_data = '%s'%(line_data)
+            line_data = join_data_ele.join([str(el) for el in line_data_elements])
+            line_data = '%s' % (line_data)
             invoice_line_element.append(line_data)
             line_count = line_count + 1
         invoice_lines = join_seg_ele.join(invoice_line_element)
@@ -80,61 +78,60 @@ class SyncDocumentType(models.Model):
     def make_invoice_x12_flatfile_data(self, invoice):
         order = invoice.invoice_line_ids.mapped('sale_line_ids.order_id')
         order_id = order[0] if order else False
-        join_seg_ele = '%s'%(self.segment_terminator)
-        join_data_ele = '%s'%(self.data_ele_separator)
-        header_data_elements= [
+        join_seg_ele = '%s' % (self.segment_terminator)
+        join_data_ele = '%s' % (self.data_ele_separator)
+        header_data_elements = [
             'H', '810',
             invoice.partner_id.id,
             invoice.name,
             invoice.invoice_date.strftime(EDI_DATE_FORMAT),
             order_id.name if order_id else '',
             order_id.date_order.strftime(EDI_DATE_FORMAT) if order_id else '',
-            '', # Bill of Lading
-            '', # Carrier Pro 
-            '', # SCAC
-            order.carrier_id.name if (order_id and order.carrier_id)  else '',
-            invoice.partner_shipping_id.name, 
+            '',  # Bill of Lading
+            '',  # Carrier Pro
+            '',  # SCAC
+            order.carrier_id.name if (order_id and order.carrier_id) else '',
+            invoice.partner_shipping_id.name,
             invoice.partner_shipping_id.street or '',
             invoice.partner_shipping_id.street2 or '',
             invoice.partner_shipping_id.city or '',
             invoice.partner_shipping_id.state_id.name or '',
             invoice.partner_shipping_id.zip or '',
             invoice.partner_shipping_id.country_id.name or '',
-            '', #The Ship To Code - The Store# where the product is shipping to
-            invoice.partner_id.name, 
+            '',  # The Ship To Code - The Store# where the product is shipping to
+            invoice.partner_id.name,
             invoice.partner_id.street or '',
             invoice.partner_id.street2 or '',
             invoice.partner_id.city or '',
             invoice.partner_id.state_id.name or '',
             invoice.partner_id.zip or '',
             invoice.partner_id.country_id.name or '',
-            '', # Bill to Code
-            '', # Ship Date
+            '',  # Bill to Code
+            '',  # Ship Date
             invoice.invoice_payment_term_id .name or '',
             (invoice.invoice_date_due - invoice.invoice_date).days if invoice.invoice_date and invoice.invoice_date_due else 0,
-            0, # Discount Days Due
-            0, # Discount Percent
-            '', # Note
-            0, # Weight
-            0, # Total Cases Shipped
+            0,  # Discount Days Due
+            0,  # Discount Percent
+            '',  # Note
+            0,  # Weight
+            0,  # Total Cases Shipped
             invoice.amount_tax,
             invoice.amount_total,
-            0, # Charge Amount2
-            0, # Allowance Percent1
-            0, # Allowance Amount1
-            0, # Allowance Percent2
-            0, # Allowance Amount2
+            0,  # Charge Amount2
+            0,  # Allowance Percent1
+            0,  # Allowance Amount1
+            0,  # Allowance Percent2
+            0,  # Allowance Amount2
         ]
 
         final_row_data = [
-            join_data_ele.join([ str(ex) for ex in  header_data_elements]),
+            join_data_ele.join([str(ex) for ex in header_data_elements]),
         ]
         invoice_data = join_data_ele.join(final_row_data)
-        invoice_data = '%s'%(invoice_data)
+        invoice_data = '%s' % (invoice_data)
         invoice_lines = self.make_invoice_line_flatfile_data(invoice)
         flat_invoice = join_seg_ele.join([invoice_data, invoice_lines, "\r\n"])
         return flat_invoice
-
 
     def _do_export_invoice_flat(self, conn, sync_action_id, values):
         '''
@@ -152,7 +149,7 @@ class SyncDocumentType(models.Model):
         if invoices:
             for invoice in invoices:
                 invoice_data = self.make_invoice_x12_flatfile_data(invoice)
-                print (invoice_data)
+                print(invoice_data)
             # TODO : used upload method of sftp
             # tmp_dir = tempfile.mkdtemp()
             # filename = r'PO' + invoice.name.replace('/', '') + '.txt'
