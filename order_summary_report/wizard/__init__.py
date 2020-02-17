ac8deaa546b8dac8d7d1bb476c4a47e6f683e1b9
dhagantrenditions <57232670+dhagantrenditions@users.noreply.github.com>
2020-02-17 15:26:01 -0500

    Add files via upload

---
+++ order_summary_report/wizard/__init__.py
@@ -0,0 +1,6 @@
+# ======================================================
+#     @Author: KSOLVES India Private Limited
+#     @Email: sales@ksolves.com
+# ======================================================
+
+from . import order_summary_report
