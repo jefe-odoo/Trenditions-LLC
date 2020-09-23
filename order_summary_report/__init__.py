fda95936d15b6001ee8a46c7f55ea2ea8f7fa345
dhagantrenditions <57232670+dhagantrenditions@users.noreply.github.com>
2020-09-23 10:20:02 -0400

    Delete __init__.py

--- order_summary_report/__init__.py
+++
@@ -1,6 +0,0 @@
-# ======================================================
-#     @Author: KSOLVES India Private Limited
-#     @Email: sales@ksolves.com
-# ======================================================
-
-from . import wizard
