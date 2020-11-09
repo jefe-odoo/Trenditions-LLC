1070d191dc0f1fedd261ba9db37221081e9d17a0
dhagantrenditions <57232670+dhagantrenditions@users.noreply.github.com>
2020-11-09 11:31:47 -0500

    Add files via upload

---
+++ order_summary_report_v10/order_summary_report/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# ======================================================
+#     @Author: KSOLVES India Private Limited
+#     @Email: sales@ksolves.com
+# ======================================================
+{
+    'name': "Trendition: Order Summary Report",
+    'summary': """
+        Prepares Order Summary Report 
+        """,
+    'description': """
+    """,
+    'author': "Ksolves India Pvt. Ltd.",
+    'license': 'OPL-1',
+    'website': "https://www.ksolves.com",
+    'maintainer': 'Ksolves India Pvt. Ltd.',
+    'live_test_url': '',
+    'category': 'Stock',
+    'version': '1.0.1',
+    'support': 'sales@ksolves.com',
+    'depends': ['base', 'stock', 'purchase', 'sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'report/order_summary_report.xml',
+        'wizard/order_summary_report_view.xml',
+        'views/views.xml',
+
+    ],
+
+}
