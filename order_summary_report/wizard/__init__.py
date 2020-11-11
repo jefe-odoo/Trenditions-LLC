62e63ebe5bc154cf3c09e45f750dec368a393a88
dhagantrenditions <57232670+dhagantrenditions@users.noreply.github.com>
2020-11-11 08:13:20 -0500

    Delete __init__.py

--- order_summary_report/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# ======================================================
-#     @Author: KSOLVES India Private Limited
-#     @Email: sales@ksolves.com
-# ======================================================
-
-from . import order_summary_report
