6a0884900481dae5abca6790f26b4ffae9381835
greg-trenditions <gtillotson@trenditions.com>
2021-11-05 14:37:23 -0500

    update

--- order_summary_report/wizard/order_summary_report.py
+++ order_summary_report/wizard/order_summary_report.py
@@ -146,7 +146,7 @@ class TrenditionOrderWarehouseReport(models.Model):
                 "Select x_studio_expected_arrival_date "\
                 "FROM purchase_order "\
                 "WHERE "\
-                "id and partner_id in (select order_id, partner_id from purchase_order_line where product_id in (select id from product_product where default_code LIKE '%s')) "\
+                "id, partner_id in (select order_id, partner_id from purchase_order_line where product_id in (select id from product_product where default_code LIKE '%s')) "\
                 "and (state = 'draft' or state = 'sent')" % (product.default_code))
                 expected_delivery_date = cr.fetchall()
                 #expected_delivery_date = expected_delivery_dates[0]
