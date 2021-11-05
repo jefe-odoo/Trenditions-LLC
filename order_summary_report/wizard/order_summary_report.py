06644c1a2adcd89c30ceb1f61051bc18b5fc2290
greg-trenditions <gtillotson@trenditions.com>
2021-11-05 15:54:43 -0500

    more updates

--- order_summary_report/wizard/order_summary_report.py
+++ order_summary_report/wizard/order_summary_report.py
@@ -149,15 +149,13 @@ class TrenditionOrderWarehouseReport(models.Model):
                 "partner_id in (select partner_id from purchase_order_line where product_id in (select id from product_product where default_code LIKE %s)) "\
                 "and id in (select order_id from purchase_order_line where product_id in (select id from product_product where default_code LIKE %s)) "\
                 "and (state = 'draft' or state = 'sent')" , (product.default_code, product.default_code))
-                expected_delivery_dates = cr.fetchall()
+                expected_delivery_dates = cr.fetchone()
                 if expected_delivery_dates:
-                    expected_delivery_date = expected_delivery_dates[0]
                     for i in expected_delivery_dates:
-                        for j in range(len(i)):
-                            if i[j] < date.today():
-                                continue
-                            if i[j] < expected_delivery_date:
-                                expected_delivery_date = [i][j]
+                        if i < date.today():
+                            continue
+                        if i < expected_delivery_date:
+                            expected_delivery_date = [i][j]
                 else:
                     expected_delivery_date = expected_delivery_dates
 
