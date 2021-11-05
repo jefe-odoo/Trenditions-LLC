3f785f1ca9918d34c67d523788e3befa44512ff0
greg-trenditions <gtillotson@trenditions.com>
2021-11-05 13:44:26 -0500

    more changes

--- order_summary_report/wizard/order_summary_report.py
+++ order_summary_report/wizard/order_summary_report.py
@@ -4,7 +4,7 @@
 # ======================================================
 
 from odoo import api, fields, models
-import datetime
+from datetime import date
 
 class TrenditionOrderWarehouseReport(models.Model):
     _name = "trendition.order.summary.report"
