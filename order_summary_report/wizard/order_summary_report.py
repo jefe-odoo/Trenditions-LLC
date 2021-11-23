ed43fd2956b2a876070f9b9df49abc7860f82872
Greg Tillotson <gtillotson@trenditions.onmicrosoft.com>
2021-11-23 12:28:02 -0600

    Changing Qty on Hand column to Qty Available #109

--- order_summary_report/wizard/order_summary_report.py
+++ order_summary_report/wizard/order_summary_report.py
@@ -9,6 +9,7 @@ from datetime import date
 class TrenditionOrderWarehouseReport(models.Model):
     _name = "trendition.order.summary.report"
     _description = "Order Summary Report"
+    _inherit = 'product.product'
 
     @api.model
     def _default_warehouse_id(self):
