ce20b52799acd59bd09c053d5fe864d90a0bd179
Greg Tillotson <gtillotson@trenditions.onmicrosoft.com>
2021-11-30 11:04:17 -0600

    Changing Qty on Hand column to Qty Available #152

--- order_summary_report/wizard/order_summary_report.py
+++ order_summary_report/wizard/order_summary_report.py
@@ -143,8 +143,8 @@ class TrenditionOrderWarehouseReport(models.Model):
                 #New code for changing On Hand Qty column to Qty Available column
                 cr = self.env.cr
                 cr.execute(
-                "Select product_uom_qty "\
-                "FROM stock_move_line "\
+                "Select product_qty "\
+                "FROM sale_order_line "\
                 "WHERE "\
                 "(state = 'confirmed' or state = 'assigned') and "\
                 "product_id in (select id from product_product where default_code = '%s')" % (product.default_code))
