7d4dcf30ccca8343a76e1fd1a14f9e964e8caf95
Greg Tillotson <gtillotson@trenditions.onmicrosoft.com>
2021-12-01 12:30:10 -0600

    Changing Qty on Hand column to Qty Available #172

--- order_summary_report/wizard/order_summary_report.py
+++ order_summary_report/wizard/order_summary_report.py
@@ -159,7 +159,7 @@ class TrenditionOrderWarehouseReport(models.Model):
                 "Select product_uom_qty "\
                 "FROM stock_move "\
                 "WHERE "\
-                "name LIKE 'WH/IN%%' "
+                "name LIKE '%%WH/IN%%' "
                 "and (state = 'confirmed' or state = 'partially_available' or state = 'assigned') "\
                 "and product_id in (select id from product_product where default_code = '%s')" % (product.default_code))
                 product_uom = cr.fetchall()
